ce56bc3b660413a540610eb179391884f432f5b3
Freyja <freyamarika@gmail.com>
2024-11-06 15:07:27 +1100

    delete win32/build directory

--- win32/build/include/h/root_directory.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROOT_DIRECTORY_H
-#define ROOT_DIRECTORY_H
-
-// Define the root path for resources
-#define ROOT_DIRECTORY "C:/Users/Freyja/Documents/GitHub/OpenGL"
-
-const char * logl_root = "C:/Users/Freyja/Documents/GitHub/OpenGL";
-
-#endif // ROOT_DIRECTORY_H
